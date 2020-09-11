bfe9d4b715b77d3e5067c5f87147653e9607ae0b
CJ-2 <56313495+CJ-2@users.noreply.github.com>
2020-09-12 02:29:34 +0300

    make it better

--- insight/settings.py
+++ insight/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = '*6st(_hgwa&z#$fssuk@qm0#93$^p135@%kffc00-tng(7jq%&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.infinitymarket.store' '127.0.0.1']
+ALLOWED_HOSTS = ['infinitymarket.store' '127.0.0.1']
 
 # Application definition
 
